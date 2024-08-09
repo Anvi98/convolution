135e4e2419468c805f51fe8540123e64763ede5c
Anvi Alex <epononanvialex@gmail.com>
2024-08-08 18:12:36 -0600

    delete html and latex files from Doxygen

--- html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['conv_2ecpp_2',['conv.cpp',['../conv_8cpp.html',1,'']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['convolution_20program_3',['Convolution Program',['../md_README.html',1,'']]]
-];
